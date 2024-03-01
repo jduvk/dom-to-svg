cbd4ad66e2cb072520007c7692df591cf8eec3dd
jduvk <159763540+jduvk@users.noreply.github.com>
2024-03-01 15:27:57 +0000

    Add custom entry point script

---
+++ dom-to-svg.js
@@ -0,0 +1,8 @@
+import { elementToSVG, inlineResources } from './lib/index.js'
+
+window.takeSVGScreenshot = async function takeSVGScreenshot(cssSelector){
+	const svgDocument = elementToSVG(document.querySelector('camos-html5client').shadowRoot.querySelector(cssSelector));
+	await inlineResources(svgDocument.documentElement);
+	const svgString = new XMLSerializer().serializeToString(svgDocument);
+	callHTMLClient(JSON.stringify({ Sender: "TakeSVGScreenshot", Data: svgString }));
+}
